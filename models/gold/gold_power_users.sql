62cbed34c445ce06704fdf62c67b8824085c48f5
chrishawnm <45526483+chrishawnm@users.noreply.github.com>
2025-07-26 13:33:57 +0000

    moving up

---
+++ models/gold/gold_power_users.sql
@@ -0,0 +1,10 @@
+SELECT
+  r.*,
+  CASE
+    WHEN r.engagement_score >= p.p75 THEN 'High'
+    WHEN r.engagement_score >= p.p50 THEN 'Medium'
+    ELSE 'Low'
+  END AS engagement_tier
+FROM {{ref('silver_monthly_user_ranked')}} r
+JOIN {{ref('silver_monthly_user_percentiles')}} p
+  ON r.mon_year = p.mon_year
